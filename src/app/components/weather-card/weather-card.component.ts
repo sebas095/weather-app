e5222f484f296e75aa2c77079162d939852e54a1
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-04-22 03:17:48 -0500

    [ADD] weather card component added

---
+++ src/app/components/weather-card/weather-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Weather } from "src/app/structures/weather.structure";
+
+@Component({
+  selector: "app-weather-card",
+  templateUrl: "./weather-card.component.html",
+  styleUrls: ["./weather-card.component.sass"],
+})
+export class WeatherCardComponent implements OnInit {
+  @Input() weather: Weather;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
