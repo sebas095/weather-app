301921ea05966f3188a2c79db6271c916aa66210
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-04-22 03:19:27 -0500

    [ADD] current weather component added

---
+++ src/app/components/current-weather/current-weather.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+import { CurrentWeatherService } from "../../services/current-weather.service";
+import { showUp } from "../../animations/showUp.animation";
+
+@Component({
+  selector: "app-current-weather",
+  templateUrl: "./current-weather.component.html",
+  styleUrls: ["./current-weather.component.sass"],
+  animations: [showUp()],
+})
+export class CurrentWeatherComponent implements OnInit {
+  constructor(public weatherService: CurrentWeatherService) {}
+
+  ngOnInit(): void {
+    //this.weatherService.weather$.subscribe(console.log);
+  }
+}
