4f970e2b628962bf3e38daa6158c23608dcd2e46
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-04-22 03:18:25 -0500

    [ADD] loading component added

---
+++ src/app/components/loading/loading.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from "@angular/core";
+import { loadingAnimation } from "../../animations/loading.animation";
+import { CurrentWeatherService } from "../../services/current-weather.service";
+
+@Component({
+  selector: "app-loading",
+  templateUrl: "./loading.component.html",
+  styleUrls: ["./loading.component.sass"],
+  animations: [loadingAnimation()],
+})
+export class LoadingComponent implements OnInit {
+  private _elements: string[] = [
+    "#ffe5ec",
+    "#ff80a0",
+    "#ff2e63",
+    "#800020",
+    "#1a0006",
+  ];
+  public elements: string[];
+
+  constructor(public readonly currentWeatherService: CurrentWeatherService) {}
+
+  ngOnInit(): void {
+    this.set();
+  }
+
+  set() {
+    this.elements = this._elements;
+    this.scheduleNextIteration();
+  }
+
+  clear() {
+    this.elements = [];
+    this.scheduleNextIteration();
+  }
+
+  scheduleNextIteration() {
+    setTimeout(() => {
+      if (this.elements.length === 0) return this.set();
+      this.clear();
+    }, 100 * this.elements.length + 350);
+  }
+}
